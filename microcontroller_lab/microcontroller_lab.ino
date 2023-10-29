1ca1dd314d98ae0c3e303cc2b7db154cd1556a41
leandroebner <leandro.ebner@outlook.de>
2023-10-29 16:01:50 +0100

    added files

---
+++ microcontroller_lab/microcontroller_lab.ino
@@ -0,0 +1,15 @@
+const int led = 3 ;
+const int button = 2 ;
+
+void setup() {
+
+pinMode(led, OUTPUT);
+pinMode(button, INPUT_PULLUP);
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
