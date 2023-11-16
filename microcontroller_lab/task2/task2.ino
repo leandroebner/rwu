6aebc45753ac99bba44bfc051c06e7607678a6e8
Leandro Ebner <leandro.ebner@outlook.de>
2023-11-16 22:47:33 +0100

    corrected missing if condition

--- microcontroller_lab/task2/task2.ino
+++ microcontroller_lab/task2/task2.ino
@@ -18,6 +18,7 @@ pinMode(button, INPUT_PULLUP);     //maps the button to physical input pin with 
 pinMode(inbuilt_led, OUTPUT);      //maps the inbuild led of the arduino nano as an output (heartbeat function)
 pinMode(pwm_input, INPUT);         //maps the pwm signal at led as input
 Serial.begin(9600);                //opens serial connection to computer
+counter = 130;
 
 }
 
@@ -29,7 +30,7 @@ if(sample == 0 && memory == 1 && millis() - ref1 >= debounce){       //triggers 
   output = !output;                                                  //toggles the virtual output
   ref1 = millis();}                                                  //creates timestamp to keep track of upcoming debouncing
 
-if(millis() - ref2 >= 10){                                           //triggers counter if time increment has passed
+if(millis() - ref2 >= -1){                                           //triggers counter if time increment has passed
   if(output == 1 && counter < 255){counter++;}                       //if ramping conditions are met, increase the brightness
   else{counter = 0;}                                                 //reset brightness if necessary
   ref2 = millis();}                                                  //creates timestamp to time couning
