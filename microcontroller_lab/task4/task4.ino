6eb175848baaff686c0cd0f3676b46a2f9a81147
Leandro Ebner <leandro.ebner@outlook.de>
2023-12-01 02:53:12 +0100

    changed direction

--- microcontroller_lab/task4/task4.ino
+++ microcontroller_lab/task4/task4.ino
@@ -4,7 +4,7 @@
 Servo myservo;
 
 void setup() {
-  
+
 IrReceiver.begin(2);
 myservo.attach(9);
 
@@ -13,8 +13,8 @@ myservo.attach(9);
 void loop() {
 
 if (IrReceiver.decode()){
-  if (IrReceiver.decodedIRData.command == 0x45) {myservo.write(5);}
-  if (IrReceiver.decodedIRData.command == 0x46) {myservo.write(175);}
+  if (IrReceiver.decodedIRData.command == 0x45) {myservo.write(175);}
+  if (IrReceiver.decodedIRData.command == 0x46) {myservo.write(5);}
   if (IrReceiver.decodedIRData.command == 0x47) {}
 IrReceiver.resume();}
 
